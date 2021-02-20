65a8a4f0011f39c235275c937fe12b48fd76e8ec
unknown <davidjmartin@hotmail.com>
2021-02-20 17:12:43 +0000

    added random entrypoint and container based tests for the controller

--- ecosystem/number-service/src/main/java/com/ecosystem/numberservice/controller/NumberServiceController.java
+++ ecosystem/number-service/src/main/java/com/ecosystem/numberservice/controller/NumberServiceController.java
@@ -1,10 +1,10 @@
 package com.ecosystem.numberservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @Slf4j
 @RestController
@@ -12,12 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class NumberServiceController {
 
     public static final String BASE_PATH = "/number-service";
+    public static final String RANDOM_NUMBER_URL = "/random";
+    public static final int UPPER_BOUNDARY = 100;
 
     @GetMapping
     @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     public String getGreeting() {
         log.info("received GET request to {}.", BASE_PATH);
         return "success. number-service is online.";
+    }
+
+    @GetMapping(RANDOM_NUMBER_URL)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public int getRandomNumber() {
+        log.info("received GET request to {}.", RANDOM_NUMBER_URL);
+        return new Random().nextInt(UPPER_BOUNDARY);
     }
 
 }
---
+++ ecosystem/number-service/src/test/java/com/ecosystem/numberservice/controller/NumberServiceControllerTests.java
@@ -0,0 +1,59 @@
+package com.ecosystem.numberservice.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class NumberServiceControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void given_basePathUrl_when_getRequestOnBasePath_then_OK() throws Exception {
+
+        // given
+        String url = NumberServiceController.BASE_PATH;
+        String expectedResponse = "success. number-service is online.";
+
+        // when then
+        ResultActions response = mockMvc.perform(get(url));
+
+        // then
+        response
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", is(expectedResponse)));
+
+    }
+
+    @Test
+    public void given_randomNumberUrl_when_getRequestOnRandomNumberUrl_then_OK() throws Exception {
+
+        // given
+        String url = NumberServiceController.BASE_PATH + NumberServiceController.RANDOM_NUMBER_URL;
+
+        // when then
+        ResultActions response = mockMvc.perform(get(url));
+
+        // then
+        response
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", lessThan(NumberServiceController.UPPER_BOUNDARY)));
+
+    }
+
+}
