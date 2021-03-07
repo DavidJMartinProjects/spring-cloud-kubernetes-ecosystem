c59c2510575d027d910387a997e2ec3effc7bf5e
unknown <davidjmartin@hotmail.com>
2021-03-07 14:04:17 +0000

    added leaderboard-service as a Kafka consumer - see notes.txt for kafka k8 deployment notes

--- ecosystem/leaderboard-service/src/test/java/ecosystem/leaderboardservice/LeaderboardServiceApplicationTests.java
+++ ecosystem/leaderboard-service/src/test/java/ecosystem/leaderboardservice/LeaderboardServiceApplicationTests.java
@@ -3,10 +3,12 @@ package ecosystem.leaderboardservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EmbeddedKafka(controlledShutdown = true, topics = "leaderboard")
 public class LeaderboardServiceApplicationTests {
 
 	@Test
--- ecosystem/leaderboard-service/src/test/java/ecosystem/leaderboardservice/controller/LeaderboardControllerTests.java
+++ ecosystem/leaderboard-service/src/test/java/ecosystem/leaderboardservice/controller/LeaderboardControllerTests.java
@@ -4,6 +4,7 @@ import ecosystem.leaderboardservice.LeaderboardServiceApplicationTests;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
