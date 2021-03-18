5fc0b3e61ea760f9a371d7dd6362e642d0b1a54c
unknown <davidjmartin@hotmail.com>
2021-03-18 16:25:14 +0000

    fixed datatable dependencies

--- ecosystem/frontend-gui/src/app/register/register.component.ts
+++ ecosystem/frontend-gui/src/app/register/register.component.ts
@@ -1,13 +1,17 @@
+import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
 
   dtOptions: DataTables.Settings = {
     searching: false,
